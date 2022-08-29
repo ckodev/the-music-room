5ea1e431a55e43367b5a70ad6e11fd07b3174b1e
ckodev <cowens17@my.bcit.ca>
2022-08-29 08:23:13 -0700

    add CPT and TAX in functions

--- functions.php
+++ functions.php
@@ -176,3 +176,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Custom Post Types & Taxonomies.
+ */
+require get_template_directory() . '/inc/cpt-taxonomy.php';
+
