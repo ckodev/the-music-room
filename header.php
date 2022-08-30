9692a9fa2815a43b88403b6b78acf48f5228795c
ckodev <cowens17@my.bcit.ca>
2022-08-29 23:00:16 -0700

    add burger menu to header-nav

--- header.php
+++ header.php
@@ -26,27 +26,24 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tmr' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tmr_description = get_bloginfo( 'description', 'display' );
-			if ( $tmr_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tmr_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		
 
+		
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tmr' ); ?></button>
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				?>
+				<a href="<?php echo esc_url( get_page_link( 31 ) ); ?>"><h1 class="page-title"><?php the_title(); ?></h1></a>
+			</div><!-- .site-branding -->
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+				<?php esc_html_e( '', 'tmr' ); ?>
+				  <span class="hamburger-icon" id="hamburger-icon">
+                      <span class="line"></span>
+                      <span class="line"></span>
+                      <span class="line"></span>
+                  </span>
+			</button>
 			<?php
 			wp_nav_menu(
 				array(
@@ -56,4 +53,6 @@
 			);
 			?>
 		</nav><!-- #site-navigation -->
+
+
 	</header><!-- #masthead -->
