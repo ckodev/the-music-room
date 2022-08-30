67390a61ace0f6f61f7a803372eadee203f0ad22
ckodev <cowens17@my.bcit.ca>
2022-08-30 10:22:57 -0700

    create template part for hero image

---
+++ template-parts/page-hero.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package tmr
+ */
+
+?>
+
+    <section class="hero-container">
+        <?php 
+        $feat_image_url = wp_get_attachment_url( get_post_thumbnail_id() );
+        $image_id = get_post_thumbnail_id();
+        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+        ?> 
+        <img src="<?php echo $feat_image_url; ?>" alt="<?php echo $image_alt; ?>">
+
+        <?php 
+        if (function_exists('get_fields')) {
+            if (get_field('hero_text')) {
+                ?>
+                <h2><?php the_field('hero_text'); ?></h2>
+                <?php
+            }
+        }
+        ?>
+    </section>
+
+
